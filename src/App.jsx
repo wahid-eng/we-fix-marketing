52b07ae4fb5f8bf95c31c4ae4eda5944d79f00c7
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-02 05:49:47 +0600

    next step section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
+import Cards from './components/Cards';
 export default function App() {
-	return (
-		<div className="container">
-			<div className="row mt-5">
-				<div className="col">
-					<button className="btn btn-black">
-						GET IN TOUCH <img src="/images/arrow-right-up.svg" alt="Arrow" />
-					</button>
-				</div>
-				<div className="col">
-					<button className="btn btn-gradient">
-						GET IN TOUCH <img src="/images/arrow-right-up.svg" alt="Arrow" />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+	return <Cards />;
 }
