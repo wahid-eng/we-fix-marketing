903ba579fc691ab05d454a76bdf46079f043d917
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-02 03:52:30 +0600

    button designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 export default function App() {
 	return (
 		<div className="container">
-			<div className="row">
-				<div className="col">Application</div>
+			<div className="row mt-5">
+				<div className="col">
+					<button className="btn btn-black">
+						GET IN TOUCH <img src="/images/arrow-right-up.svg" alt="Arrow" />
+					</button>
+				</div>
+				<div className="col">
+					<button className="btn btn-gradient">
+						GET IN TOUCH <img src="/images/arrow-right-up.svg" alt="Arrow" />
+					</button>
+				</div>
 			</div>
 		</div>
 	);
