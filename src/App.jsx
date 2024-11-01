32ce21a7a96ad2db14d82cfb8cf95349b373ba90
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-02 01:46:55 +0600

    font, formatter setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 export default function App() {
 	return (
-		<div className='container'>
-			<div className='row'>
-				<div className='col'>Application</div>
+		<div className="container">
+			<div className="row">
+				<div className="col">Application</div>
 			</div>
 		</div>
 	);
