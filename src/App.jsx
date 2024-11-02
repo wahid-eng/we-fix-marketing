6c98912ed322dcd6ec7ea115527f80ee0f8a21a6
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-02 08:38:20 +0600

    guarantee section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Approach from './components/Approach';
 import Cards from './components/Cards';
 import Footer from './components/Footer';
+import Guarantee from './components/Guarantee';
 import Story from './components/Story';
 export default function App() {
 	return (
 		<>
+			<Guarantee />
 			<Story />
 			<Approach />
 			<Cards />
---
+++ src/components/Guarantee.jsx
@@ -0,0 +1,40 @@
+export default function Guarantee() {
+	return (
+		<section className="bg-gradient py-5">
+			<div className="container py-5">
+				<div className="row section-heading text-center text-md-end fw-bolder mb-5 mb-md-0">
+					<div className="col">
+						<p className="fw-bolder lh-1 mb-0 display-1">OUR GUARANTEE</p>
+					</div>
+				</div>
+				<div className="row row-cols-1 row-cols-md-2 row-gap-5 gx-lg-5 align-items-center">
+					<div className="col">
+						<img
+							src="/images/guarantee.webp"
+							alt="Our guarantee"
+							className="w-100"
+							loading="lazy"
+						/>
+					</div>
+					<div className="col">
+						<p className="fw-bolder display-5">WE OFFER THE GUARANTEE!</p>
+						<p>
+							We are so confident in our services that we offer this guarantee:
+						</p>
+						<p className="fw-bold">
+							If you do not see better leads, more sales, and a higher ROI
+							within 90 days, we will work for free until you do.
+						</p>
+						<p>We stand by our promise to help your business grow.</p>
+						<div className="pt-4">
+							<a href="#" className="btn btn-gradient">
+								BOOK YOUR CALL
+								<img src="/images/arrow-right-up.svg" alt="Arrow" />
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
