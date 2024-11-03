5afe2ae2230a3690b627bce7a6d534ad8f11de11
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-03 07:30:36 +0600

    mobile navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
 							/>
 						</svg>
 					</a>
-					<div className="collapse navbar-collapse" id="navbar">
+					<div className="collapse navbar-collapse" id="navbarCollapse">
 						<ul className="navbar-nav mx-auto gap-3">
 							<li className="nav-item">
 								<a className="nav-link lh-1 fw-medium text-center" href="#">
@@ -66,11 +66,78 @@ export default function Header() {
 						GET IN TOUCH
 						<img src="/images/arrow-right-up.svg" alt="Arrow" />
 					</a>
-					<button className="navbar-toggler pe-0" type="button">
+					<button
+						className="navbar-toggler pe-0"
+						data-bs-toggle="offcanvas"
+						data-bs-target="#navbarOffcanvas"
+						type="button"
+					>
 						<span className="navbar-toggler-icon"></span>
 					</button>
 				</div>
 			</nav>
+
+			<div
+				className="offcanvas offcanvas-start text-bg-dark"
+				id="navbarOffcanvas"
+			>
+				<div className="offcanvas-header">
+					<a href="#" className="offcanvas-title">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="150"
+							height="51"
+							fill="#fff"
+						>
+							<path d="M8.853 26.574L.378.091h9.194l6.848 22.36h-4.616L19.068.091h8.21l6.772 22.36h-4.427l7.15-22.36h8.475l-8.475 26.483h-9.572L21.679 8.263h2.573l-5.826 18.311H8.853zm46.55-16.647h11.804v6.432H55.404V9.927zm.605 9.912H69.25v6.734H47.232V.091h21.527v6.734h-12.75V19.84zm36.345-8.475h11.652v6.734H92.354v-6.734zm.605 15.209H84.03V.091h21.528v6.734H92.959v19.749zm15.419 0V.091h8.929v26.483h-8.929zm10.768 0l11.804-16.495-.038 6.243L119.486.091h10.026l6.659 9.837-4.275.038 6.507-9.875h9.61l-11.426 15.89V9.776l11.993 16.798h-10.215l-6.658-10.291 4.123-.038-6.507 10.329h-10.177zM2.931 50.751V33.933h1.033l7.808 13.479h-.553l7.784-13.479h1.009l.024 16.818h-1.201l-.024-14.968h.336l-7.376 12.734h-.601l-7.4-12.734h.36v14.968H2.931zm20.159 0l7.736-16.818h1.225l7.736 16.818h-1.321l-7.304-16.049h.529l-7.28 16.049H23.09zm2.835-4.733l.408-1.033H36.4l.408 1.033H25.925zm16.916 4.733V33.933h6.03c1.41 0 2.619.224 3.628.673s1.786 1.097 2.331 1.946c.545.833.817 1.85.817 3.051 0 1.169-.272 2.178-.817 3.027-.545.833-1.321 1.482-2.331 1.946-1.009.448-2.218.673-3.628.673h-5.358l.553-.577v6.079h-1.225zm11.676 0l-4.373-6.103h1.393l4.373 6.103h-1.394zm-10.451-6.006l-.553-.577h5.358c1.81 0 3.179-.4 4.108-1.201.945-.801 1.417-1.922 1.417-3.364 0-1.458-.472-2.579-1.417-3.364-.929-.801-2.298-1.201-4.108-1.201h-5.358l.553-.601v10.307zm17.124 1.754l-.048-1.538 10.715-11.028h1.49l-7.496 7.76-.697.745-3.964 4.06zm-1.057 4.253V33.933h1.225v16.818h-1.225zm12.229 0l-7.568-8.649.841-.889 8.217 9.538h-1.49zm5.745-9.082h9.106v1.081h-9.106V41.67zm.144 7.977H88.63v1.105H77.026V33.933H88.27v1.105H78.252v14.608zm18.066 1.105V35.038h-6.055v-1.105h13.335v1.105h-6.055v15.713h-1.225zm10.318 0V33.933h1.226v16.818h-1.226zm7.086 0V33.933h1.033l11.989 15.305h-.553V33.933h1.226v16.818h-1.009l-12.013-15.304h.552v15.304h-1.225zm26.752.12c-1.266 0-2.435-.208-3.508-.625-1.073-.432-2.01-1.033-2.811-1.802a8.17 8.17 0 0 1-1.826-2.691c-.433-1.041-.649-2.178-.649-3.412s.216-2.363.649-3.388a7.94 7.94 0 0 1 1.85-2.715 8.46 8.46 0 0 1 2.787-1.778c1.073-.432 2.25-.649 3.532-.649 1.217 0 2.346.192 3.387.577s1.93.961 2.667 1.73l-.769.817c-.736-.721-1.545-1.233-2.426-1.538-.865-.304-1.81-.456-2.835-.456-1.089 0-2.099.184-3.028.553a7.27 7.27 0 0 0-2.426 1.562 7.26 7.26 0 0 0-1.61 2.355c-.368.897-.552 1.874-.552 2.931s.184 2.034.552 2.931a7.26 7.26 0 0 0 1.61 2.355 7.27 7.27 0 0 0 2.426 1.562c.929.369 1.939.553 3.028.553a8.55 8.55 0 0 0 2.811-.456c.897-.304 1.714-.809 2.45-1.514l.697.937c-.753.721-1.658 1.265-2.715 1.634-1.041.352-2.138.529-3.291.529zm4.829-2.282v-6.175h1.177v6.295l-1.177-.12z" />
+						</svg>
+					</a>
+					<button
+						type="button"
+						className="btn-close btn-close-white"
+						data-bs-dismiss="offcanvas"
+					></button>
+				</div>
+				<div className="offcanvas-body">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								THE STRUGGLE
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								THE SOLUTION
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								SUCCESS STORIES
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								OUR GUARANTEE
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								ORIGIN STORY
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								UNIQUE APPROACH
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link link-light py-3 fw-medium" href="#">
+								NEXT STEP
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</header>
 	);
 }
